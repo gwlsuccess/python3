d78100197eda3ab524d39cb2f72f840f69026321
xwp <xwp_fullstack@163.com>
2018-06-01 21:59:56 +0800

    集成flask_session扩展包指定session信心存储到redis中

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
+# 导入redis 数据库模块
+from redis import StrictRedis
+
 class Config:
     DEBUG = True
+    # 设置密钥
+    SECRET_KEY='LPyirIeIv0lJVsLrUl2QLA=='
     # 配置sqlalchemy连接mysql数据库
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/flask_day08'
     # 配置数据库的动态追踪修改
     SQLALCHEMY_TRACK_MODIFICATIONS =False
+    # 配置redis的主机和端口
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    # 使用redis 用于保存session数据
+    SESSION_TYPE ='redis'
+    # 对象session信息进行签名
+    SESSION_USE_SIGNER =  True
+    # 存储session的redis实例
+    SESSION_REDIS= StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 指定session的过期时间1天
+    PERMANENT_SESSION_LIFETIME = 86400
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,session
 # 导入　flask_scrip　管理器，在终端执行程序
 from flask_script import Manager
 # 导入迁移扩展包和迁移命令
@@ -7,12 +7,16 @@ from flask_migrate import Migrate,MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 # 导入配置文件
 from config import Config
+# 导入扩展flask_session,可以配置session信息的存储
+from flask_session import Session
 
 app=Flask(__name__)
 # 实例化数据库对象
 db = SQLAlchemy(app)
 # 使用配置对象
 app.config.from_object(Config)
+# 实例化session对象
+Session(app)
 # 实例化管理器对象
 manager = Manager(app)
 # 使用迁移框架
@@ -22,6 +26,8 @@ manager.add_command('db',MigrateCommand)
 
 @app.route('/')
 def index():
+    # 请求对象session
+    session['name']=2018
     return 'index'
 
 if __name__ == '__main__':
