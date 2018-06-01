fc2d68c22fc7ba565c15dc497a2bbb7001b750ff
xwp <xwp_fullstack@163.com>
2018-06-01 20:39:38 +0800

    添加配置文件

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 from flask import Flask
-
+from flask_script import Manager
+from config import Config
 app=Flask(__name__)
+
+app.config.from_object(Config)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return 'index'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    # 必须配置项目参数　runserver
+    manager.run()
