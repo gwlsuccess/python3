c0d83b8cbd410880358832f8ae57b166f7566f1c
xwp <xwp_fullstack@163.com>
2018-06-01 20:58:12 +0800

    实例化数据库对象，使用迁移框架

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 class Config:
     DEBUG = True
+    # 配置sqlalchemy连接mysql数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/flask_day08'
+    # 配置数据库的动态追踪修改
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
--- manage.py
+++ manage.py
@@ -1,11 +1,24 @@
 from flask import Flask
+# 导入　flask_scrip　管理器，在终端执行程序
 from flask_script import Manager
+# 导入迁移扩展包和迁移命令
+from flask_migrate import Migrate,MigrateCommand
+# 导入flask_sqlalchemy数据库扩展包
+from flask_sqlalchemy import SQLAlchemy
+# 导入配置文件
 from config import Config
+
 app=Flask(__name__)
-
+# 实例化数据库对象
+db = SQLAlchemy(app)
+# 使用配置对象
 app.config.from_object(Config)
-
+# 实例化管理器对象
 manager = Manager(app)
+# 使用迁移框架
+Migrate(app,db)
+# 通过管理器对象集成迁移命令
+manager.add_command('db',MigrateCommand)
 
 @app.route('/')
 def index():
