4e1d5c5def75b1615d029a2c2ac8e9ed7e4149e5
xwp <xwp_fullstack@163.com>
2018-06-02 22:29:13 +0800

    导入模型类和常量配置文件＆加载小图标

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import session,render_template
+from flask import session,render_template,current_app
 # 导入蓝图对象
 from . import news_blue
 # 使用蓝图对象
@@ -7,3 +7,10 @@ def index():
     # 请求对象session
     session['name']=2018
     return render_template('news/index.html')
+
+# 加载项目小图标
+@news_blue.route('/favicon.ico')
+def favicon():
+    # 静态路径访问的默认实现，send_static_file
+    # 把静态文件发送给浏览器
+    return current_app.send_static_file('news/favicon.ico')
