659e7ccd0085fbe0ee97fe25f2b2866edb1b0823
xwp <xwp_fullstack@163.com>
2018-06-07 08:27:27 +0800

    新闻首页列表数据的加载

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,11 +1,12 @@
-from flask import session,render_template,current_app,jsonify
+from flask import session,render_template,current_app,jsonify,request
 # 导入蓝图对象
 from . import news_blue
 # 导入模型类User 和 News
 from info.models import User,News,Category
 # 导入自定义状态码
 from info.utils.response_code import RET
-
+# 导入常量定义文件
+from info import constants
 # 使用蓝图对象
 @news_blue.route('/')
 def index():
@@ -61,6 +62,48 @@ def index():
     }
 
     return render_template('news/index.html',data=data)
+
+# 新闻列表数据的显示　
+@news_blue.route('/news_list')
+
+def get_news_list():
+     cid = request.args.get('cid','1')
+     page = request.args.get('page','1')
+     per_page = request.args.get('per_page','10')
+     try:
+        cid,page,per_page  = int(cid),int(page),int(per_page)
+     except Exception as e:
+         current_app.logger.error(e)
+         return jsonify(errno =RET.PARAMERR,errmsg ='参数格式错误')
+     # 根据分类id查询数据库
+     filters = []
+     # 此处将查询到的所有数据显示在更新页面上，当id = 1
+     # 当id > 1时，查询的是其他分类的内容
+     if cid >1:
+         filters.append(News.category_id == cid)
+     try:
+         # 默认按照新闻分类进行过滤，按照新闻发布时间倒序排序，分页每页10条
+         paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,constants.HOME_PAGE_MAX_NEWS,False)
+     except Exception as e:
+         current_app.logger.error(e)
+         return jsonify(errno =RET.DBERR,errmsg ='数据库访问失败')
+
+     news_list = paginate.items
+     total_page = paginate.pages
+     current_page = paginate.page
+     # 定义容器，遍历分页后的新闻对象，转成字典
+     news_dict_list = []
+     for news in news_list:
+         news_dict_list.append(news.to_dict())
+     data ={
+         'news_dict_list':news_dict_list,
+         'total_page':total_page,
+         'current_page':current_page
+     }
+     # 返回结果
+     return jsonify(errno =RET.OK,errmsg ='OK',data = data)
+
+# 新闻页面详情
 
 # 加载项目小图标
 @news_blue.route('/favicon.ico')
